c0993e36d9a53dc49c54bcad3dd78ead4f073415
Soumyadip1245 <soumyadeepdas2002@gmail.com>
2024-04-06 10:53:27 +0530

    Theme Changes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css"
 import App from "./App"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { ThemeProvider, createTheme } from "@mui/material"
-const storedThemeColor = localStorage.getItem('theme-color')
-const themeColor = storedThemeColor || 'red-theme'
-document.body.classList.add(themeColor);
+const storedThemeColor = localStorage.getItem("theme-color")
+const themeColor = storedThemeColor || "red-theme"
+document.body.classList.add(themeColor)
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const queryClient = new QueryClient()
@@ -25,11 +25,7 @@ const theme = createTheme({
 root.render(
   <ThemeProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
-      <div style={{ height: "100vh", width: "100%", overflow: "auto" }}>
-        <div className="custom-scrollbar" style={{ height: "100%", width: "100%" }}>
-          <App />
-        </div>
-      </div>
+      <App />
     </QueryClientProvider>
   </ThemeProvider>
 )
