4d4b59b95dbe1de2f34751a1d141ad10db407002
Soumyadip1245 <soumyadeepdas2002@gmail.com>
2024-05-18 11:56:37 +0530

    Backend Url

--- src/utils/backendUrl.js
+++ src/utils/backendUrl.js
@@ -1,1 +1,1 @@
-export const rootUrl = process.env.NODE_ENV === "production" ? "https://online-stores-compare-backend.vercel.app" : ""
+export const rootUrl = process.env.NODE_ENV === "production" ? process.env.REACT_APP_BACKEND_URL : "http://localhost:8080"
